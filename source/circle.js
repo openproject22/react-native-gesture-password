0c397eef7d432f14a7f8ad7dea3624b3a74b04d8
openproject22 <100454949+openproject22@users.noreply.github.com>
2022-03-11 09:48:51 +0200

    Update circle.js
    outer circle - dark  background

--- source/circle.js
+++ source/circle.js
@@ -56,6 +56,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#010101",
   },
   inner: {
     backgroundColor: "#8E91A8",
